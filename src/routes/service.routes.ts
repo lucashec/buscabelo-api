96c475fd097193a8524e29957da4a5f62efbf78e
José Victor <jose.t@academico.ifrn.edu.br>
2021-06-15 22:13:19 -0300

    rotas de update, delete e getById do serviço

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -4,6 +4,10 @@ import ServiceController from '../app/controllers/ServiceController';
 
 const serviceRouter = Router();
 
-serviceRouter.post('/', ServiceController.create)
+serviceRouter.post('/', ServiceController.create);
+serviceRouter.get('/', ServiceController.getAll);
+serviceRouter.get('/:id', ServiceController.getById);
+serviceRouter.put('/:id', ServiceController.update);
+serviceRouter.delete('/:id', ServiceController.remove);
 
 export default serviceRouter;
