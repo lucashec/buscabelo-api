56738a3fee7c096ce63afab4c1139c4a70f41584
José Victor <jose.t@academico.ifrn.edu.br>
2021-06-29 17:13:16 -0300

    criando documentaçao de rotas existentes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
 import cors from 'cors';
+import SwaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 import './database/connect';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-doc", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
+
 app.use(routes);
 app.use(cors())
 
