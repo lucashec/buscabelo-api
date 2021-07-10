1b426a4051e002e202f3229300290e66d6cb233b
Lucas Henrique Evaristo da Costa <h.lucas@academico.ifrn.edu.br>
2021-07-10 16:18:08 -0300

    upload com armazenamento local

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes/index';
 import cors from 'cors';
+import uploadConfig from './config/upload';
 import SwaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swagger.json';
 
@@ -17,6 +18,7 @@ app.use(express.json());
 
 app.use(cors());
 app.use("/api-doc", SwaggerUi.serve, SwaggerUi.setup(swaggerDocs));
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(process.env.PORT || 3000, () => console.log('Server started at http://localhost:3000/v1'));
