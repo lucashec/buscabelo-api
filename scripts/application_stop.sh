5642dcc6fc053caa570e15edaeda64ea3483e691
Lucas Henrique <lucashenriqueev@gmail.com>
2022-03-05 13:27:02 -0300

    bash

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-DIR="/home/ubuntu/API"
 FILE="/home/ubuntu/API/docker-compose.yaml"
 
-if [ -d "$DIR" ] && [ -f "$FILE" ]; then {
+if [ -f "$FILE" ]; then {
     echo "Stopping the containers"
-    cd $DIR && sudo docker-compose down
-}
-    
+    sudo docker-compose down
+}    
 else
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
+    echo "No containers found"
 fi
 
